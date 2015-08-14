e25bdfa509ea848ae8fe5ef4114e0bbaa4a3c476
Craig Hesling <craig@hesling.com>
2015-08-14 11:33:45 -0400

    Add check for BASH version 4. Does nothing is not at least version 4.

--- font_simple.sh
+++ font_simple.sh
@@ -1,6 +1,7 @@
 # Version: 1
 # Created: 10/30/2014
 # Updated: 12/11/2014 (just removed the IFVERB)
+# Updated: 8/14/2015 (checks that the BASH version is at least 4)
 # Author:  Craig Hesling
 ## Standard Font Used In Scripts ##
 
@@ -15,17 +16,23 @@ B="\E[01m" # bold
 U="\E[04m" # underscore
 I="\E[07m" # invert colors
 
-declare -g -A FONTS=( )
-FONTS=(              \
-	[off]="$OF"      \
-	[red]="$RD"      \
-	[green]="$GN"    \
-	[yellow]="$YL"   \
-	[blue]="$BL"     \
-	[bold]="$B"      \
-	[underline]="$U" \
-	[invert]="$I"    \
-)
+if [ "$(echo $BASH_VERSION | cut -d '.' -f 1)" -ge 4 ]; then
+	declare -g -A FONTS=( )
+	FONTS=(              \
+		[off]="$OF"      \
+		[red]="$RD"      \
+		[green]="$GN"    \
+		[yellow]="$YL"   \
+		[blue]="$BL"     \
+		[bold]="$B"      \
+		[underline]="$U" \
+		[invert]="$I"    \
+	)
+	ASSOC_SUPPORT=1
+else
+	ASSOC_SUPPORT=0
+	echo "font_simple.sh: Error - BASH version does not support associative arrays" >&2
+fi
 
 # Font Control #
 
@@ -35,6 +42,11 @@ FONTS=(              \
 # Ex. font off
 font()
 {
+	# do nothing for old versions of BASH
+	if [ $ASSOC_SUPPORT -lt 1 ]; then
+		return
+	fi
+
 	for opt; do
 		#IFVERB2 echo "font: $opt" >&2
 		printf "${FONTS[$opt]}"
