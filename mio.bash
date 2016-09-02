c3c604337fce36bfd61c1e4fddc82edc8026f2af
Craig Hesling <craig@hesling.com>
2016-09-02 05:45:29 -0400

    Added more functionality to mio.bash

--- mio.bash
+++ mio.bash
@@ -32,17 +32,59 @@ meta_edit() {
 	cat $tmp | meta_set $node
 }
 
+# ref_get craignode > ref.xml
+ref_get() {
+	local node=$1
+	get_item $node references \
+	| xmllint --format --xpath "/*[local-name() = 'iq']/*[local-name() = 'pubsub']/*[local-name() = 'items']/*[local-name() = 'item']/*[local-name() = 'references']" - \
+	| xml_prettyprint
+	echo
+}
+
+# cat ref.xml | ref_set craignode
+ref_set() {
+	local node=$1
+	publish $node references -
+}
+
+# ref_edit craignode
+ref_edit() {
+	local node=$1
+
+	local tmp=`tempfile /tmp/refXXX.xml`
+	ref_get $node > $tmp
+	vi $tmp
+	cat $tmp | ref_set $node
+}
+
+# storage_get craignode > storage.xml
+storage_get() {
+	local node=$1
+	get_item $node storage \
+	| xmllint --format --xpath "/*[local-name() = 'iq']/*[local-name() = 'pubsub']/*[local-name() = 'items']/*[local-name() = 'item']/*[local-name() = 'addresses']" - \
+	| xml_prettyprint
+	echo
+}
+
 # Setup BASH Completions #
 
 if (( COMPLEX_COMPLETIONS_ENABLED )); then
 	complete -F _single_node meta_get
 	complete -F _single_node meta_set
 	complete -F _single_node meta_edit
+	complete -F _single_node ref_get
+	complete -F _single_node ref_set
+	complete -F _single_node ref_edit
+	complete -F _single_node storage_get
 else
 	{
 	complete -r meta_get
 	complete -r meta_set
 	complete -r meta_edit
+	complete -r ref_get
+	complete -r ref_set
+	complete -r ref_edit
+	complete -r storage_get
 	} 2> /dev/null
 fi
 
